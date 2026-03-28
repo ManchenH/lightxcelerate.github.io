9092282e37e726202d1cf81b1b5e4de6ac9364e8
ManchenH <87054556+ManchenH@users.noreply.github.com>
2026-03-27 18:17:30 -0700

    Delete nav.js

--- nav.js
+++
@@ -1,7 +0,0 @@
-// Highlight the active page in the navbar
-const currentPage = window.location.pathname.split("/").pop();
-document.querySelectorAll("nav a").forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
-});
